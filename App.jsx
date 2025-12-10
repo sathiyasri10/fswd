7ee3d84314b4edbb487c33bbe0bf4735ff7f8710
sathiya sri <sathiyasri1011@gmail.com>
2025-12-10 16:51:16 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import CourseRegistration from "./Fullstackproject/Sample/CourseRegistration";
+
+
+function App() {
+  return (
+    <>
+      <CourseRegistration/>
+    </>
+  );
+}
+
+export default App;
