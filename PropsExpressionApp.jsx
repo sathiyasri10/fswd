36dc656558a8d577056fd56bb70a4cb4f79e0a18
sathiya sri <sathiyasri1011@gmail.com>
2025-12-18 15:01:11 +0530

    Delete PropsExpressionApp.jsx

--- PropsExpressionApp.jsx
+++
@@ -1,4 +0,0 @@
-import PropsExpression from "./Fullstack/React/PropsExpression";
-export default function App(){
-  return<PropsExpression title="Full Stack Web Development" tutor="Handled by Ponmalar.S"/>;
-}
