093a90fe8aa8fb262966a7f16265532c955afac7
sathiya sri <sathiyasri1011@gmail.com>
2025-12-18 15:01:01 +0530

    Delete PropsFunctionApp.jsx

--- PropsFunctionApp.jsx
+++
@@ -1,4 +0,0 @@
-import PropsFunction from "./Fullstack/React/PropsFunction";
-export default function App(){
-  return<PropsFunction title="Full Stack Web Development" tutor="Handled by Ponmalar.S"/>;
-}
